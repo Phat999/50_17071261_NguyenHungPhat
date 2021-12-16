f1d85126249cf0234c627a7aa1799711ed717cc3
Phat999 <64886764+Phat999@users.noreply.github.com>
2021-12-16 16:44:03 +0700

    Cau 3

---
+++ CAU3/src/main/java/com/example/cau3/Cau3Application.java
@@ -0,0 +1,13 @@
+package com.example.cau3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cau3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cau3Application.class, args);
+    }
+
+}
